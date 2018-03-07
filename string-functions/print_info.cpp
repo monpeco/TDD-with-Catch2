8492578386811c94492d97b8294d3db9796d3860
monpeco <monpeco@gmail.com>
2018-03-07 14:03:53 +0000

    Add test cases (base and others)

--- string-functions/print_info.cpp
+++ string-functions/print_info.cpp
@@ -8,14 +8,19 @@ std::string print_info(std::string* stringArray, unsigned int size);
 
 TEST_CASE("print simple","print a simple string based on a parameter"){
   std::string s[] = {"hi", "there", "here", "another"};
+  REQUIRE( print_info(s,0) == ",");
   REQUIRE( print_info(s,1) == "hi,");
   REQUIRE( print_info(s,2) == "hi,there,");
+  REQUIRE( print_info(s,3) == "hi,there,here,");
+  REQUIRE( print_info(s,4) == "hi,there,here,another,");
   
 }
 
 
 std::string print_info(std::string* stringArray, unsigned int size){
   std::string result{""};
+  
+  if (size==0) result = ",";
   
   for(int i=0; i<size; i++){
     result += (stringArray[i]) + ",";
