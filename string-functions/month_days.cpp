7d60eefb3f6ca53cd3c5ddcd2ca0359a67b16dfc
monpeco <monpeco@gmail.com>
2018-03-09 13:33:51 +0000

    Add financial month test case

--- string-functions/month_days.cpp
+++ string-functions/month_days.cpp
@@ -7,7 +7,7 @@
 std::string print_months(std::string file_name);
 
 TEST_CASE("Test print_months()", "Print the name and number days of the monts in the months file"){
-  REQUIRE( print_months("infile.txt") ==  "There are 31 days in January."
+  REQUIRE( print_months("months.txt") ==  "There are 31 days in January."
                                           "There are 28 days in February."
                                           "There are 31 days in March."
                                           "There are 30 days in April."
@@ -21,9 +21,29 @@ TEST_CASE("Test print_months()", "Print the name and number days of the monts in
                                           "There are 31 days in December.");
 }
 
+TEST_CASE("Test print_months() with financial months", "Print the name and number days of the monts in the financial months file"){
+  REQUIRE( print_months("months_f.txt") ==  "There are 30 days in January."
+                                            "There are 28 days in February."
+                                            "There are 30 days in March."
+                                            "There are 30 days in April."
+                                            "There are 30 days in May."
+                                            "There are 30 days in June."
+                                            "There are 30 days in July."
+                                            "There are 30 days in August."
+                                            "There are 30 days in September."
+                                            "There are 30 days in October."
+                                            "There are 30 days in November."
+                                            "There are 30 days in December.");
+}
+
+/*  String -> String
+*   Produces the formatted output of the months' info stored in the months.txt file
+*/  
 std::string print_months(std::string file_name){
   return "";
 }
+
+
 
 /*
 
