cae4ec9c93958d6cebbaf953594d080a3a458142
monpeco <monpeco@gmail.com>
2018-03-15 19:32:36 +0000

    Change examples to use epsilon

--- unit-convertion/miles_to_kilometers.cpp
+++ unit-convertion/miles_to_kilometers.cpp
@@ -3,13 +3,16 @@
 #include <iostream>
 #include "catch.hpp"
 
+const int epsilon = 0.00001;
 
 double miles_to_kms(double _miles);
 
 
 TEST_CASE("Test miles_to_kms", "Given a distance in miles, returns the distance in Kilometers"){
-  REQUIRE( miles_to_kms(0.0) ==  0.0);
-  REQUIRE( miles_to_kms(5.0) ==  8.0467);
+  REQUIRE( miles_to_kms(0.0) - 0.0 <= epsilon);
+  REQUIRE( miles_to_kms(3.0) - 4.8280 <= epsilon);
+  REQUIRE( miles_to_kms(5.0) - 8.0467 <= epsilon);
+  REQUIRE( miles_to_kms(-9.0) - (-14.4840) <= epsilon);
 }
 
 /*  double -> double
