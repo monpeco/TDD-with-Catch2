c840594f5cdeb18460be3d7ff95408de8d1bd0bf
monpeco <monpeco@gmail.com>
2018-03-15 19:59:37 +0000

    Return epsilon and Fix its definition

--- unit-convertion/miles_to_kilometers.cpp
+++ unit-convertion/miles_to_kilometers.cpp
@@ -4,22 +4,21 @@
 #include <cmath>
 #include "catch.hpp"
 
-const int epsilon = 0.00001;
+const double epsilon = 0.0001;
 
 double miles_to_kms(double _miles);
 
 
 TEST_CASE("Test miles_to_kms", "Given a distance in miles, returns the distance in Kilometers"){
-  REQUIRE( fabs(miles_to_kms(0.0) - 0.0) <= std::numeric_limits<double>::epsilon());
-  REQUIRE( fabs(miles_to_kms(3.0) - 4.8280) <= std::numeric_limits<double>::epsilon());
-  REQUIRE( fabs(miles_to_kms(5.0) - 8.0467) <= std::numeric_limits<double>::epsilon());
-  REQUIRE( fabs(miles_to_kms(-9.0) - (-14.4840)) <= std::numeric_limits<double>::epsilon());
+  REQUIRE( fabs(miles_to_kms(0.0) - 0.0) <= epsilon);
+  REQUIRE( fabs(miles_to_kms(3.0) - 4.8280) <= epsilon);
+  REQUIRE( fabs(miles_to_kms(5.0) - 8.0467) <= epsilon);
+  REQUIRE( fabs(miles_to_kms(-9.0) - (-14.4840)) <= epsilon);
 }
 
 /*  double -> double
 *   Transform a distance in Miles to its equivalent in Kilometers
 */
 double miles_to_kms(double _miles){
-
-  return 0;
+  return _miles * 1.609344;
 }
