d36aee4748068042723c0ad6a1e664c627296b9d
monpeco <monpeco@gmail.com>
2018-03-15 20:06:46 +0000

    Add kms_to_miles signature, porpuse, stub and examples

--- unit-convertion/miles_to_kilometers.cpp
+++ unit-convertion/miles_to_kilometers.cpp
@@ -7,7 +7,7 @@
 const double epsilon = 0.0001;
 
 double miles_to_kms(double _miles);
-
+double kms_to_miles(double _kms);
 
 TEST_CASE("Test miles_to_kms", "Given a distance in miles, returns the distance in Kilometers"){
   REQUIRE( fabs(miles_to_kms(0.0) - 0.0) <= epsilon);
@@ -21,4 +21,20 @@ TEST_CASE("Test miles_to_kms", "Given a distance in miles, returns the distance 
 */
 double miles_to_kms(double _miles){
   return _miles * 1.609344;
+}
+
+
+
+TEST_CASE("Test kms_to_miles", "Given a distance in Kilometers, returns the distance in Miles"){
+  REQUIRE( fabs(kms_to_miles(0.0) - 0.0) <= epsilon);
+  REQUIRE( fabs(kms_to_miles(3.0) - 1.8641) <= epsilon);
+  REQUIRE( fabs(kms_to_miles(5.0) - 3.1068) <= epsilon);
+  REQUIRE( fabs(kms_to_miles(-9.0) - (-5.5923)) <= epsilon);
+}
+
+/*  double -> double
+*   Transform a distance in Kilometers to its equivalent in Miles
+*/
+double kms_to_miles(double _kms){
+  return 0;
 }
