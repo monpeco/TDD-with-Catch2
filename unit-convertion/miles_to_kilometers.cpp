9c2046e77e47836473e56032f19a17adc35a154f
monpeco <monpeco@gmail.com>
2018-03-15 19:50:36 +0000

    Change epsilon to std::numeric_limits<double>::epsilon

--- unit-convertion/miles_to_kilometers.cpp
+++ unit-convertion/miles_to_kilometers.cpp
@@ -1,6 +1,7 @@
 #define CATCH_CONFIG_MAIN
 
 #include <iostream>
+#include <cmath>
 #include "catch.hpp"
 
 const int epsilon = 0.00001;
@@ -9,10 +10,10 @@ double miles_to_kms(double _miles);
 
 
 TEST_CASE("Test miles_to_kms", "Given a distance in miles, returns the distance in Kilometers"){
-  REQUIRE( miles_to_kms(0.0) - 0.0 <= epsilon);
-  REQUIRE( miles_to_kms(3.0) - 4.8280 <= epsilon);
-  REQUIRE( miles_to_kms(5.0) - 8.0467 <= epsilon);
-  REQUIRE( miles_to_kms(-9.0) - (-14.4840) <= epsilon);
+  REQUIRE( fabs(miles_to_kms(0.0) - 0.0) <= std::numeric_limits<double>::epsilon());
+  REQUIRE( fabs(miles_to_kms(3.0) - 4.8280) <= std::numeric_limits<double>::epsilon());
+  REQUIRE( fabs(miles_to_kms(5.0) - 8.0467) <= std::numeric_limits<double>::epsilon());
+  REQUIRE( fabs(miles_to_kms(-9.0) - (-14.4840)) <= std::numeric_limits<double>::epsilon());
 }
 
 /*  double -> double
