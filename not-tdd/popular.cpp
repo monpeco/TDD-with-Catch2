d63216a749c9999c1e33a72cb67e5577bf2850b8
monpeco <monpeco@gmail.com>
2018-03-18 23:02:17 +0000

    Create a fast version of popular scaffold

---
+++ not-tdd/popular.cpp
@@ -0,0 +1,23 @@
+/*
+int popular(int numbers_size, int *numbers){
+	// complete
+}
+
+Ej: 10, [1, 3, 5, 6, 3, 6, 7, 8, 9, 6] => 6 (six is the most popular number)
+*/
+
+#include <iostream>
+
+int popular(int numbers_size, int *numbers);
+
+int main(){
+  
+  int array1[] = {1, 3, 5, 6, 3, 6, 7, 8, 9, 6};
+  int length1{10};
+  
+  return 0;
+}
+
+int popular(int numbers_size, int *numbers){
+	return 0;
+}
