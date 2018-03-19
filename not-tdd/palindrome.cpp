fae91429b058f691cae3635813c5f32a294aa42d
monpeco <monpeco@gmail.com>
2018-03-19 14:11:53 +0000

    Add basic scaffold of palindrome.cpp

---
+++ not-tdd/palindrome.cpp
@@ -0,0 +1,27 @@
+/*
+2)	Given a list o words, return a list of word ("YES" or "NO") responding 
+if each given word is a palindrome or not.
+
+char** palindrome(int words_size, char** words) {
+	// complete
+}
+
+Ej : ["house", "radar", "cart"] => ["NO", "YES", "NO"]
+*/
+
+#include <iostream>
+
+char** palindrome(int words_size, char** words);
+
+int main(){
+  char *array0[] = {"house", "radar", "cart"};
+  int length0{3};
+
+  return 0;
+}
+
+char** palindrome(int words_size, char** words) {
+	char** ppBuffer = nullptr;
+	
+	return ppBuffer;
+}
