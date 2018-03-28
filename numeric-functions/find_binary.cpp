03d5aeba908f19202b2ddd2f90d8cc16280fee34
monpeco <monpeco@gmail.com>
2018-03-28 14:37:20 +0000

    Create scaffold for find_binary

---
+++ numeric-functions/find_binary.cpp
@@ -0,0 +1,22 @@
+/*
+1)	Given a list of binary codes and a binary file return, for every code, 
+a list of index indicating which position is that code located at, into the 
+file.
+
+int** find(int codes_size, char** codes, char* file){
+	// complete
+}
+
+Ej: 2, [“001001”, “111”], “1001001001110111“ => [[1, 4][9, 13]]
+*/
+
+#define CATCH_CONFIG_MAIN
+
+#include "catch.hpp"
+
+int** find(int codes_size, char** codes, char* file);
+
+int** find(int codes_size, char** codes, char* file){
+
+	return nullptr;
+}
