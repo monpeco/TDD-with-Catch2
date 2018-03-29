76ed63d9963f7c04929903be09856c0ee4d8fbb9
monpeco <monpeco@gmail.com>
2018-03-29 19:05:54 +0000

    Add test case of 2 and 15

--- unit-convertion/decimal_to_hex.cpp
+++ unit-convertion/decimal_to_hex.cpp
@@ -12,10 +12,23 @@ TEST_CASE("Test base case", "Given a string that represents a series of decimal 
   REQUIRE( decimal_to_hex(rstr) == "");
 }
 
-TEST_CASE("Test decimal_to_hex", "Given a string that represents a series of decimal numbers, returns its hexadecimal represtation"){
+TEST_CASE("Test decimal_to_hex case of 0", "Given a string that represents a series of decimal numbers, returns its hexadecimal represtation"){
   std::string str("0");
   std::string rstr = str;
   REQUIRE( decimal_to_hex(rstr) == "0");
+}
+
+TEST_CASE("Test decimal_to_hex case of 2", "Given a string that represents a series of decimal numbers, returns its hexadecimal represtation"){
+  std::string str("2");
+  std::string rstr = str;
+  REQUIRE( decimal_to_hex(rstr) == "2");
+}
+
+
+TEST_CASE("Test decimal_to_hex case of 15", "Given a string that represents a series of decimal numbers, returns its hexadecimal represtation"){
+  std::string str("15");
+  std::string rstr = str;
+  REQUIRE( decimal_to_hex(rstr) == "f");
 }
 
 /*  string -> string
