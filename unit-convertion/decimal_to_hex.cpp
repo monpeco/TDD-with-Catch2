4f9debc83439a8def4bfed4f0381598fd09d8254
monpeco <monpeco@gmail.com>
2018-03-29 19:03:41 +0000

    Add test case of 0

--- unit-convertion/decimal_to_hex.cpp
+++ unit-convertion/decimal_to_hex.cpp
@@ -12,6 +12,11 @@ TEST_CASE("Test base case", "Given a string that represents a series of decimal 
   REQUIRE( decimal_to_hex(rstr) == "");
 }
 
+TEST_CASE("Test decimal_to_hex", "Given a string that represents a series of decimal numbers, returns its hexadecimal represtation"){
+  std::string str("0");
+  std::string rstr = str;
+  REQUIRE( decimal_to_hex(rstr) == "0");
+}
 
 /*  string -> string
 *   Given a string that represents a series of decimal numbers, returns its hexadecimal represtation
