94ef32cd139e180ec24a859d8468e3355ec1b223
monpeco <monpeco@gmail.com>
2018-04-02 21:26:33 +0000

    Add find() test case

--- numeric-functions/find_binary.cpp
+++ numeric-functions/find_binary.cpp
@@ -16,11 +16,22 @@ Ej: 2, [“001001”, “111”], “1001001001110111“ => [[1, 4][9, 13]]
 
 int** find(int codes_size, char** codes, char* file);
 
+
+TEST_CASE("Test 1: int** find(int, char**, char*)", "Test 1"){
+  char code_array[2][10] = {"001001", "111"};
+  int code_length{2};
+  char binary_line[] = "1001001001110111";
+  char *pbinary_line = binary_line;
+  //find(code_length, code_array,pbinary_line);
+  //REQUIRE( find(code_length, code_array) == 3 );
+}
+
 /* int, char**, char* -> int**
 *  Given a list of binary codes, inspects a binary file and Return the index 
 *  indicating the position where is a coincidence
 */
 int** find(int codes_size, char** codes, char* file){
-
+  
 	return nullptr;
 }
+
